4fadb3c178d005a70736d08ff021ad8da9a89686
Stefan Tsankov <stsankov@shopmetrics.com>
2020-03-12 17:41:01 +0200

    Added footer

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,147 @@
 <div>
     <div class="container">
         <div class="row">
+          <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+               <div class="card card-blog">
+                <div class="card-image">
+                  <a href="#pablo">
+                    <img class="img" src="https://picsum.photos/200/150/?random">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    Good Design Is Aesthetic
+                  </h4>
+                  <div class="card-description">
+                    Cards are an interaction model that are spreading pretty widely, in fact. What all of these have in common is that they're pulling...
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+                    <a href="#pablo" class="btn btn-danger btn-round">View Article</a>
+                  </div>
+                  <hr>
+                  <div class="card-footer">
 
+                    <div class="author">
+                      <a href="#pablo">
+                        <img src="https://picsum.photos/200/150/?random" alt="..." class="avatar img-raised">
+                        <span>Mike John</span>
+                      </a>
+                    </div>
+                    <div class="stats">
+                      <i class="fa fa-clock-o" aria-hidden="true"></i> 5 min read
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+             <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+               <div class="card card-blog">
+                <div class="card-image">
+                  <a href="#pablo">
+                    <img class="img" src="https://picsum.photos/200/150/?random">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    Good Design Is Aesthetic
+                  </h4>
+                  <div class="card-description">
+                    Cards are an interaction model that are spreading pretty widely, in fact. What all of these have in common is that they're pulling...
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+                    <a href="#pablo" class="btn btn-danger btn-round">View Article</a>
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+
+                    <div class="author">
+                      <a href="#pablo">
+                        <img src="https://picsum.photos/200/150/?random" alt="..." class="avatar img-raised">
+                        <span>Mike John</span>
+                      </a>
+                    </div>
+                    <div class="stats">
+                      <i class="fa fa-clock-o" aria-hidden="true"></i> 5 min read
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+             <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+               <div class="card card-blog">
+                <div class="card-image">
+                  <a href="#pablo">
+                    <img class="img" src="https://picsum.photos/200/150/?random">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    Good Design Is Aesthetic
+                  </h4>
+                  <div class="card-description">
+                    Cards are an interaction model that are spreading pretty widely, in fact. What all of these have in common is that they're pulling...
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+                    <a href="#pablo" class="btn btn-danger btn-round">View Article</a>
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+
+                    <div class="author">
+                      <a href="#pablo">
+                        <img src="https://picsum.photos/200/150/?random" alt="..." class="avatar img-raised">
+                        <span>Mike John</span>
+                      </a>
+                    </div>
+                    <div class="stats">
+                      <i class="fa fa-clock-o" aria-hidden="true"></i> 5 min read
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+             <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+               <div class="card card-blog">
+                <div class="card-image">
+                  <a href="#pablo">
+                    <img class="img" src="https://picsum.photos/200/150/?random">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    Good Design Is Aesthetic
+                  </h4>
+                  <div class="card-description">
+                    Cards are an interaction model that are spreading pretty widely, in fact. What all of these have in common is that they're pulling...
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+                    <a href="#pablo" class="btn btn-danger btn-round">View Article</a>
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+
+                    <div class="author">
+                      <a href="#pablo">
+                        <img src="https://picsum.photos/200/150/?random" alt="..." class="avatar img-raised">
+                        <span>Mike John</span>
+                      </a>
+                    </div>
+                    <div class="stats">
+                      <i class="fa fa-clock-o" aria-hidden="true"></i> 5 min read
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
         <div class="row">
 
@@ -40,95 +180,117 @@
                 </div>
               </div>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-                <div class="card">
 
-                    <div class="card-block">
-                        <figure class="profile">
-                            <img src="https://picsum.photos/200/150/?random" class="profile-avatar" alt="">
-                        </figure>
-                        <h4 class="card-title mt-3">Tawshif Ahsan Khan</h4>
-                        <div class="meta">
-                            <a>Friends</a>
-                        </div>
-                        <div class="card-text">
-                            Tawshif is a web designer living in Bangladesh.
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <small>Last updated 3 mins ago</small>
-                        <button class="btn btn-secondary float-right btn-sm">show</button>
-                    </div>
+             <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+               <div class="card card-blog">
+                <div class="card-image">
+                  <a href="#pablo">
+                    <img class="img" src="https://picsum.photos/200/150/?random">
+                  </a>
                 </div>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    Good Design Is Aesthetic
+                  </h4>
+                  <div class="card-description">
+                    Cards are an interaction model that are spreading pretty widely, in fact. What all of these have in common is that they're pulling...
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+                    <a href="#pablo" class="btn btn-danger btn-round">View Article</a>
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+
+                    <div class="author">
+                      <a href="#pablo">
+                        <img src="https://picsum.photos/200/150/?random" alt="..." class="avatar img-raised">
+                        <span>Mike John</span>
+                      </a>
+                    </div>
+                    <div class="stats">
+                      <i class="fa fa-clock-o" aria-hidden="true"></i> 5 min read
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+
+
+             <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+               <div class="card card-blog">
+                <div class="card-image">
+                  <a href="#pablo">
+                    <img class="img" src="https://picsum.photos/200/150/?random">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    Good Design Is Aesthetic
+                  </h4>
+                  <div class="card-description">
+                    Cards are an interaction model that are spreading pretty widely, in fact. What all of these have in common is that they're pulling...
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+                    <a href="#pablo" class="btn btn-danger btn-round">View Article</a>
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+
+                    <div class="author">
+                      <a href="#pablo">
+                        <img src="https://picsum.photos/200/150/?random" alt="..." class="avatar img-raised">
+                        <span>Mike John</span>
+                      </a>
+                    </div>
+                    <div class="stats">
+                      <i class="fa fa-clock-o" aria-hidden="true"></i> 5 min read
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+             <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+               <div class="card card-blog">
+                <div class="card-image">
+                  <a href="#pablo">
+                    <img class="img" src="https://picsum.photos/200/150/?random">
+                  </a>
+                </div>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    Good Design Is Aesthetic
+                  </h4>
+                  <div class="card-description">
+                    Cards are an interaction model that are spreading pretty widely, in fact. What all of these have in common is that they're pulling...
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+                    <a href="#pablo" class="btn btn-danger btn-round">View Article</a>
+                  </div>
+                  <hr>
+                  <div class="card-footer">
+
+                    <div class="author">
+                      <a href="#pablo">
+                        <img src="https://picsum.photos/200/150/?random" alt="..." class="avatar img-raised">
+                        <span>Mike John</span>
+                      </a>
+                    </div>
+                    <div class="stats">
+                      <i class="fa fa-clock-o" aria-hidden="true"></i> 5 min read
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
         </div>
     </div>
 
-
-
-
-      <div class="container">
-           <div class="row">
-              <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-                  <div class="card">
-                      <img class="card-img-top" src="https://picsum.photos/200/150/?random">
-                      <div class="card-block">
-                          <h5 class="text-bold">Tawshif Ahsan Khan</h5>
-                      </div>
-                  </div>
-              </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-                <div class="card">
-                    <img class="card-img-top" src="https://picsum.photos/200/150/?random
-">
-                    <div class="card-block">
-                        <h4 class="card-title">Tawshif Ahsan Khan</h4>
-                        <div class="meta">
-                            <a href="#">Friends</a>
-                        </div>
-                        <div class="card-text">
-                            Tawshif is a web designer living in Bangladesh.
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <span class="float-right">Joined in 2013</span>
-                        <span><i class=""></i>75 Friends</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-                <div class="card">
-                    <img class="card-img-top" src="https://picsum.photos/200/150/?random
-">
-                    <p class="card-block">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam velit quisquam veniam excepturi temporibus inventore corporis dicta sit culpa veritatis placeat earum, dolorum asperiores, delectus dolore voluptatibus, at magnam nobis!
-                    </p>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-                <div class="card">
-                    <img class="card-img-top" src="https://picsum.photos/200/150/?random
-">
-                    <div class="card-block">
-                        <figure class="profile">
-                            <img src="https://picsum.photos/200/150/?random" class="profile-avatar" alt="">
-                        </figure>
-                        <h4 class="card-title mt-3">Tawshif Ahsan Khan</h4>
-                        <div class="meta">
-                            <a>Friends</a>
-                        </div>
-                        <div class="card-text">
-                            Tawshif is a web designer living in Bangladesh.
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <small>Last updated 3 mins ago</small>
-                        <button class="btn btn-secondary float-right btn-sm">show</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-      </div>
 </div>
 
 
