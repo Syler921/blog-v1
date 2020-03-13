9ffbc19b5b0a98cb4817888cb31743b3f9eedd04
Stefan Tsankov <stsankov@shopmetrics.com>
2020-03-13 10:39:08 +0200

    Nav,footer,carrousel,BlogCard components created

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,171 +1,31 @@
 <template>
   <div>
-   <!-- Navigation -->
-
-
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">Start Bootstrap</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home
-                <span class="sr-only">(current)</span>
-              </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-<header>
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner" role="listbox">
-      <!-- Slide One - Set the background image for this slide in the line below -->
-      <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/LAaSoL0LrYs/1920x1080')">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4">First Slide</h2>
-          <p class="lead">This is a description for the first slide.</p>
-        </div>
-      </div>
-      <!-- Slide Two - Set the background image for this slide in the line below -->
-      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/bF2vsubyHcQ/1920x1080')">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4">Second Slide</h2>
-          <p class="lead">This is a description for the second slide.</p>
-        </div>
-      </div>
-      <!-- Slide Three - Set the background image for this slide in the line below -->
-      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/szFUQoyvrxM/1920x1080')">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4">Third Slide</h2>
-          <p class="lead">This is a description for the third slide.</p>
-        </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-  </div>
-</header>
-
-
-
+    <Navigation/>
+    <Carrousel/>
     <nuxt />
-
-    <!-- Footer -->
-<footer>
-  <div class="container">
-    <div class="row">
-
-      <div class="col-lg-4 col-md-6">
-        <h3>Site Map</h3>
-        <ul class="list-unstyled three-column">
-          <li>Home</li>
-          <li>Services</li>
-          <li>About</li>
-          <li>Code</li>
-          <li>Design</li>
-          <li>Host</li>
-          <li>Contact</li>
-          <li>Company</li>
-        </ul>
-        <ul class="list-unstyled socila-list">
-          <li><img src="http://placehold.it/48x48" alt="" /></li>
-          <li><img src="http://placehold.it/48x48" alt="" /></li>
-          <li><img src="http://placehold.it/48x48" alt="" /></li>
-          <li><img src="http://placehold.it/48x48" alt="" /></li>
-          <li><img src="http://placehold.it/48x48" alt="" /></li>
-        </ul>
-      </div>
-
-      <div class="col-lg-4 col-md-6">
-        <h3>Latest Articles</h3>
-        <div class="media">
-          <a href="#" class="pull-left">
-            <img src="http://placehold.it/64x64" alt="" class="media-object" />
-          </a>
-          <div class="media-body">
-            <h4 class="media-heading">Programming</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          </div>
-        </div>
-
-        <div class="media">
-          <a href="#" class="pull-left">
-            <img src="http://placehold.it/64x64" alt="" class="media-object" />
-          </a>
-          <div class="media-body">
-            <h4 class="media-heading">Coding</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          </div>
-        </div>
-
-        <div class="media">
-          <a href="#" class="pull-left">
-            <img src="http://placehold.it/64x64" alt="" class="media-object" />
-          </a>
-          <div class="media-body">
-            <h4 class="media-heading">Web Sesign</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          </div>
-        </div>
-
-      </div>
-
-      <div class="col-lg-4">
-        <h3>Our Work</h3>
-        <img class="img-thumbnail" src="http://placehold.it/150x100" alt="" />
-        <img class="img-thumbnail" src="http://placehold.it/150x100" alt="" />
-        <img class="img-thumbnail" src="http://placehold.it/150x100" alt="" />
-        <img class="img-thumbnail" src="http://placehold.it/150x100" alt="" />
-      </div>
-
-    </div>
-  </div>
-  <div class="copyright text-center">
-    Copyright &copy; 2017 <span>Your Template Name</span>
-  </div>
-</footer>
-<!-- Footer -->
+    <Footer/>
   </div>
 </template>
 
+<script>
+import Navigation from '~/components/Navigation.vue'
+import Carrousel from '~/components/Carrousel.vue'
+import Footer from '~/components/Footer.vue'
+import {$,jQuery} from 'jquery';
 
+
+
+export default {
+  components: {
+    Carrousel,
+    Navigation,
+    Footer
+  }
+}
+</script>
 <style>
 
-.carousel-item {
-  height: 50vh;
-  min-height: 350px;
-  background: no-repeat center center scroll;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
+
 
 
 
@@ -218,68 +78,5 @@ html {
 }
 
 
-footer {
-  background: #222;
-  color: #aaa;
-  padding-top: 10px;
-}
 
-footer a {
-  color: #aaa;
-}
-
-footer a:hover {
-  color: #fff;
-}
-
-footer h3 {
- color: #0894d1;
-  letter-spacing: 1px;
-  margin: 30px 0 20px;
-}
-
-footer .three-column {
- overflow: hidden;
-}
-
-footer .three-column li{
- width: 33.3333%;
-  float: left;
-  padding: 5px 0;
-}
-
-footer .socila-list {
-  overflow: hidden;
-  margin: 20px 0 10px;
-}
-
-footer .socila-list li {
-  float: left;
-  margin-right: 3px;
-  opacity: 0.7;
-  overflow: hidden;
-  border-radius: 50%;
-  transition: all 0.3s ease-in-out;
-}
-
-footer .socila-list li:hover {
-  opacity: 1;
-}
-
-footer .img-thumbnail {
-  background: rgba(0, 0, 0, 0.2);
-  border: 1px solid #444;
-  margin-bottom: 5px;
-}
-
-footer .copyright {
-  padding: 15px 0;
-  background: #333;
-  margin-top: 20px;
-  font-size: 15px;
-}
-
-footer .copyright span {
-  color: #0894d1;
-}
 </style>
